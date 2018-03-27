f935351c7ae4c67f79e28d96d2370d9c4d62d045
sara <sarahll.pro@gmail.com>
2018-03-27 12:10:03 +0200

    commit du 27 mars

--- src/main/java/com/formation/demo/carnet/Carnet.java
+++ src/main/java/com/formation/demo/carnet/Carnet.java
@@ -1,16 +1,41 @@
 package com.formation.demo.carnet;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
 public class Carnet {
 
 	private int id;
+	
+	//@NotNull
 	private Civilite civilite;
+	//@NotBlank              //@NotBlank verification de champs de formulaire
+	@Pattern(regexp=" (?i)[a-z]{2,50}", message="{com.formation.demo.constraint.nom.message}")//?i ignorer la case maj min//ou message="ERROR, ceci ne respecte pas les infos entrées")
 	private String nom;
+	
+	//@NotBlank //(message= "vous n'avez pas rempli le champs nom") //message personnalisé
+	@Pattern(regexp="(?i)[a-z\\- ] {2,100}", message="{com.formation.demo.constraint.prenom.message}")
 	private String prenom;
+	
+	@NotBlank
 	private String dateDeNaissance;
+	
+	//@NotBlank
+	@Pattern(regexp="(\\+[0-9]|[0] {2})[0-9]{6,14}(?:x.+]?$)", message="{com.formation.demo.constraint.tel.message}")
 	private String tel;
+	
+	//@NotBlank(message="{com.formation.demo.constraint.email.message}")//message special properties
+	@Pattern(regexp="(\\b[A-Z0-9._%-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b)", message="{com.formation.demo.constraint.email.message}")
 	private String email;
+	
+	//@NotBlank
+	@Pattern(regexp="[0-9]{5}", message="{com.formation.demo.constraint.cp.message}")
 	private String cp;
+	
+	//@NotBlank
+	@Pattern(regexp="(?i)[a-z -]{1,45}", message="{com.formation.demo.constraint.ville.message}")
 	private String ville;
+	
 	
 	//Constructeur Vide
 	public Carnet() {
--- src/main/java/com/formation/demo/carnet/CarnetController.java
+++ src/main/java/com/formation/demo/carnet/CarnetController.java
@@ -4,8 +4,14 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -19,7 +25,7 @@ public class CarnetController {
 			
 			new Carnet(3, Civilite.M, "Potter", "Harry", "1989-01-20","0202020202", "24 private Drive", "44001", "Londres")));
 	
-	// mon premier commentaire
+	// mon premier commentaire du 26.03.18
 	@GetMapping( "/carnets")
 	public List<Carnet> getAllRest() {
 		return this.listeCarnets;
@@ -28,7 +34,65 @@ public class CarnetController {
 	public ModelAndView getAll () {//Model model
 		//model.addAttribute("liste", listeCarnets);
 		ModelAndView mav= new ModelAndView("pages/carnets");
-		mav.addObject(listeCarnets)	;
+		mav.addObject("liste",listeCarnets)	;  // ici on passe dans liste la listeCarnets
 		return mav;
 	}
+	
+	@RequestMapping(value="/carnets/{id}", method=RequestMethod.GET)
+	public Carnet getById(@PathVariable Integer id) {
+    	for (Carnet c : this.listeCarnets) {
+    		if (((Integer)c.getId()).equals(id)){
+    			return c;
+    		}
+    	}
+    	return null;
+	}
+	@RequestMapping(value="/carnets/{id}", method=RequestMethod.DELETE)
+	public void DelById(@PathVariable Integer id) {
+		this.listeCarnets.removeIf(p-> ((Integer)p.getId()).equals(id));
+	}
+	
+	
+	//partie wafa(manquée le vendredi
+	
+	//@RequestMapping(value="/carnets") en brut
+	@PostMapping("/carnets") //ajout JSON avec @RequestBody
+	public void addjson(@RequestBody Carnet carnet)
+	{
+		this.listeCarnets.add(carnet);
+	}
+	// Ajout de carnet par formulaire en post
+	@RequestMapping(value="/formulaire",method=RequestMethod.POST)
+	public ModelAndView add(@Validated Carnet carnet, BindingResult bindingResult)  {   //avant  on avais @ModelAttribute //BindingResult pour afficher erreur
+	
+		if(bindingResult.hasErrors()) {
+			return new ModelAndView ("pages/formulaire.html").addObject("carnet", carnet);
+			//tant que erreur existe afficher formulaire
+			//pas de else car il ici retourne quelque chose et il sort 
+		}
+		System.out.println(carnet);
+		carnet.setId(listeCarnets.size()+1);
+		this.listeCarnets.add(carnet);
+		return new ModelAndView ("pages/carnetAffichesultat.html").addObject("carnet", carnet); //carnetAffichesultat.html nous affiche nos entrées sans erreur de champs vide
+	}
+	
+	//pour l'affichage du OldFormulaire
+	// recup formulaire créé
+	/*@GetMapping(value="/formulaire")
+	public ModelAndView form()
+	{
+		ModelAndView mav = new ModelAndView("pages/formulaire");		
+		return mav;
+	}*/
+	
+	
+	
+	// pour l'affichage du formulaire
+		@GetMapping(value="/formulaire")
+		public ModelAndView form()
+		{
+			ModelAndView mav = new ModelAndView("pages/formulaire");
+			mav.addObject("carnet", new Carnet());
+			return mav;
+		}
 	}
